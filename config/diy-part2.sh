f6690e872e5c9fdb8c5cc19234b2a004bda7ba0e
nantayo <sora1924@outlook.com>
2022-01-07 12:32:25 +0800

    N1 OpenWrt

---
+++ config/diy-part2.sh
@@ -0,0 +1,21 @@
+# Modify some code adaptation
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+
+# Add autocore support for armvirt
+sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.3.101/g' package/base-files/files/bin/config_generate
+
+# Add haproxy
+rm -f package/feeds/packages/haproxy
+svn co https://github.com/nantayo/Haproxy/trunk package/feeds/packages/haproxy
+
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk package/luci-app-amlogic
+
+# Add luci-app-dnsfilter
+svn co https://github.com/kiddin9/luci-app-dnsfilter/trunk package/luci-app-dnsfilter
+
+# Add luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
