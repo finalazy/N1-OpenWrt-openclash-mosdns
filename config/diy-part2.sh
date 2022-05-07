ed6d8c93009e57f536e9763cb59c27325c09521a
阿健 <983097239@qq.com>
2022-05-08 00:28:35 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -7,6 +7,8 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
 # Add haproxy
 rm -f package/feeds/packages/haproxy
 #svn co https://github.com/nantayo/Haproxy/trunk package/feeds/packages/haproxy
@@ -22,5 +24,9 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk package/luci-app-amlogic
 
 svn co https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash
 
+rm -rf package/lean/luci-theme-argon
+
 svn co https://github.com/jerrykuku/luci-theme-argon.git 
 svn co https://github.com/chenhw2/luci-app-aliddns.git package/feeds/luci-app-aliddns
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
