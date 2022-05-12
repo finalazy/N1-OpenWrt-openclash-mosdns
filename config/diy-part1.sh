6c9c6d7302da12af593dd0bec6731d27f110504e
阿健 <983097239@qq.com>
2022-05-12 20:04:47 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -1,2 +1,2 @@
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
