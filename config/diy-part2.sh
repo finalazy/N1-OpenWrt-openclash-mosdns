3c88e3d05a3ca755a6778d1dcae6750285316384
阿健 <983097239@qq.com>
2022-05-12 10:56:38 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -29,3 +29,6 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
