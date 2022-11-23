c84a40f99b60c3a6e492fd40a8d55666f1787e28
阿健 <983097239@qq.com>
2022-11-23 13:55:21 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -7,7 +7,7 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
-svn co https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash
+svn co https://github.com/vernesong/OpenClash.git package/feeds/luci-app-openclash
 
 svn co https://github.com/chenhw2/luci-app-aliddns.git package/feeds/luci-app-aliddns
 
@@ -15,7 +15,7 @@ rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #阿里云web dav
-git clone https://github.com/messense/aliyundrive-webdav.git package/lean/luci-app-aliyundrive-webdav
+git clone https://github.com/messense/aliyundrive-webdav.git package/feeds/luci-app-aliyundrive-webdav
 #adguardhome
-git clone https://github.com/AdguardTeam/AdGuardHome.git package/lean/luci-app-adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome.git package/feeds/luci-app-adguardhome
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
