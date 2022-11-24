a5111b9ed08be777a93ed3a2a49ee048276415da
阿健 <983097239@qq.com>
2022-11-24 23:22:32 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -1,21 +1,21 @@
 # Modify some code adaptation
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
-
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
-
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/lean/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
-svn co https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash
-
-svn co https://github.com/chenhw2/luci-app-aliddns.git package/lean/luci-app-aliddns
-
+#阿里云web dav
+git clone https://github.com/messense/aliyundrive-webdav.git package/feeds/luci-app-aliyundrive-webdav
+#adguardhome
+git clone https://github.com/AdguardTeam/AdGuardHome.git package/feeds/luci-app-adguardhome
+#openclash
+svn co https://github.com/vernesong/OpenClash.git package/feeds/luci-app-openclash
+#阿里ddns
+svn co https://github.com/chenhw2/luci-app-aliddns.git package/feeds/luci-app-aliddns
+#更换默认argon主题
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#阿里云web dav
-git clone https://github.com/messense/aliyundrive-webdav.git package/lean/luci-app-aliyundrive-webdav
-#adguardhome
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
