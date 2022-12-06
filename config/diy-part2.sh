585db34641aa63d9f0a442c47b907d1bf8adc5f7
阿健 <983097239@qq.com>
2022-12-06 16:43:41 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -5,28 +5,28 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
-#阿里云web dav
+# => 阿里云web dav
 git clone https://github.com/messense/aliyundrive-webdav.git package/luci-app-aliyundrive-webdav
 
-#adguardhome
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+# => adguardhome
+#git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 
-#openclash
+# => openclash
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-#阿里ddns
+# => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 
-#更换默认argon主题
+# => 更换默认argon主题
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
-#passwall
+# => passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b packages package/passwall_package
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b luci package/passwall
 
-#ssr plus
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+# => ssr plus
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
