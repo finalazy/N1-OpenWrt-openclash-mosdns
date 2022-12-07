53e325bc0a9bf120df06670f002924c00bb90a26
阿健 <983097239@qq.com>
2022-12-07 21:19:13 +0800

    Create diy-part2.sh

---
+++ config/diy-part2.sh
@@ -0,0 +1,16 @@
+# Modify some code adaptation
+#sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+# Add autocore support for armvirt
+sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
+
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
+
+# => 更换默认argon主题
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
