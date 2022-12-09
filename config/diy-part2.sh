ff8555198d3c780ea414d2623b8df9540cc279b4
阿健 <983097239@qq.com>
2022-12-09 09:07:48 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -5,6 +5,24 @@ sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/auto
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
 
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+
+# => 阿里云web dav
+git clone https://github.com/messense/aliyundrive-webdav.git package/luci-app-aliyundrive-webdav
+
+# => adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+
+# => 阿里ddns
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+
+# => ssr plus
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+
+# => dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
 # => 更换默认argon主题
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
