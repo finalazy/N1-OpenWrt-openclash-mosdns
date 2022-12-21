9ed87a527502f84d96f8a5b9c13cd56c6015c934
阿健 <983097239@qq.com>
2022-12-21 20:08:27 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -17,7 +17,7 @@ git clone https://github.com/messense/aliyundrive-webdav.git package/luci-app-al
 # => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 # => dockerman
-#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 # => ssr plus
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
