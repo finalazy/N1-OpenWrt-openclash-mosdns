ecaaa2a19fb013d36e574698e414f2d1d0f4a3c4
阿健 <983097239@qq.com>
2022-12-23 15:01:07 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -17,9 +17,10 @@ git clone https://github.com/messense/aliyundrive-webdav.git package/luci-app-al
 # => 阿里ddns
 git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 # => dockerman
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-# => ssr plus
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+
+# => openclash
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # => 更换默认argon主题
 rm -rf feeds/luci/themes/luci-theme-argon
