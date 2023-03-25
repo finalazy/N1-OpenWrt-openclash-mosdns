a6de4bb60d72ec61784dae7a16fe7dba1de21cd9
caojian <983097239@qq.com>
2023-03-25 12:54:12 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -2,10 +2,15 @@
 #sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generate
+
 echo '修改机器名称'
 sed -i 's/OpenWrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+
+#修改密码
+sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:19440:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 # Add luci-app-amlogic
 #svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 # 1.Set the download repository of the OpenWrt files to your github.com
