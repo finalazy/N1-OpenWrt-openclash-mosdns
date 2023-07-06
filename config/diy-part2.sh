3a753a8e4059f493383a3bf318b1515aab918c33
caojian <983097239@qq.com>
2023-07-06 09:48:22 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -9,45 +9,9 @@ sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generat
 #修改密码
 sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
-#docker 修改start--------------------------->
-# 查找并分区 U 盘
-U_DISK=""
-while read -r line; do
-    if [[ $line == *"/dev/sd"* ]]; then
-        size=$(echo "$line" | awk '{print $4}')
-        if (( ${size%G} >= 20 )); then
-            U_DISK=$(echo "$line" | awk '{print $1}')
-            break
-        fi
-    fi
-done < <(lsblk -o NAME,SIZE,MOUNTPOINT)
-
-if [ -n "$U_DISK" ]; then
-    (
-    echo o
-    echo n
-    echo p
-    echo 1
-    echo
-    echo
-    echo w
-    ) | fdisk "$U_DISK"
-
-    mkfs.ext4 "${U_DISK}1"
-    # 打印 U_DISK 变量
-    echo "U_DISK: $U_DISK"
-fi
-
-# 修改 /etc/docker/daemon.json 文件中的 data-root 参数
-sed -i 's/"data-root": "\(.*\)"/"data-root": "\/opt\/docker\/"/' /etc/docker/daemon.json
-
-# 修改 /etc/config/dockerd 文件中的 auto_start 参数
-sed -i 's/option auto_start .*/option auto_start '\''1'\''/' /etc/config/dockerd
-
-#docker 修改end--------------------------->
-
 # => 阿里云web dav
 git clone https://github.com/messense/aliyundrive-webdav.git package/luci-app-aliyundrive-webdav
+
 # => alist
 #rm -rf feeds/packages/lang/golang
 #svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
